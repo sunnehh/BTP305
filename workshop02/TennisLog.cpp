d9ed986590fcbca908c83d18116b4690c9273c5b
Sunny Qi <sunny_qi@live.ca>
2022-09-22 11:35:02 -0400

    all required functions have skeletons

--- workshop02/TennisLog.cpp
+++ workshop02/TennisLog.cpp
@@ -1,13 +1,51 @@
+#include <iomanip>
+#include <fstream>
+#include <iostream>
 #include "TennisLog.h"
 
 namespace sdds {
-
+	using namespace std;
 	TennisMatch::TennisMatch() {
-		this->match_id = {};
+		this->match_id = -1;
 		this->tournament_id = {};
 		this->tournament_name = {};
 		this->winner = {};
 		this->loser = {};
+	};
+
+	ostream& operator<<(std::ostream& os, const TennisMatch& match) {
+		if (match.match_id == -1)
+		{
+			os << "Empty Match" << endl;
+		}
+		else
+		{
+			os << right << setw(20) << setfill('.');
+			os << "Tourney ID : ";
+			os << left << setw(30) << setfill('.');
+			os << match.tournament_id << endl;
+
+			os << right << setw(20) << setfill('.');
+			os << "Match ID : ";
+			os << left << setw(30) << setfill('.');
+			os << match.match_id << endl;
+
+			os << right << setw(20) << setfill('.');
+			os << "Tourney : ";
+			os << left << setw(30) << setfill('.');
+			os << match.tournament_name << endl;
+
+			os << right << setw(20) << setfill('.');
+			os << "Winner : ";
+			os << left << setw(30) << setfill('.');
+			os << match.winner << endl;
+
+			os << right << setw(20) << setfill('.');
+			os << "Loser : ";
+			os << left << setw(30) << setfill('.');
+			os << match.loser << endl;
+		};
+		return os;
 	};
 
 	TennisLog::TennisLog() {
@@ -22,4 +60,27 @@ namespace sdds {
 			m_num_matches = 0;
 		}
 	}
+
+	TennisLog::TennisLog(string filename) {
+		// get the num of elements in the file
+		// create new matches for each entry
+		// assign the values to the matches
+		size_t lines = 0;
+		string currentline = "";
+		ifstream in(filename);
+		if (in.is_open() == false)
+		{
+			TennisLog();
+			cout << "cannot open file " << filename << endl;
+			return;
+		}
+		while (!in.eof())
+		{
+			lines++;
+
+		}
+	}
+}
+
+int main() {
 }
--- workshop02/TennisLog.h
+++ workshop02/TennisLog.h
@@ -1,6 +1,7 @@
 #ifndef SDDS_TENNIS_LOG_H
 #define SDDS_TENNIS_LOG_H
 #include <string>
+#include <iostream>
 
 namespace sdds {
 
@@ -8,23 +9,37 @@ namespace sdds {
 	{
 		std::string tournament_id;
 		std::string tournament_name;
-		unsigned int match_id;
+		int match_id;
 		std::string winner;
 		std::string loser;
 
 		TennisMatch();
 	};
-
+	std::ostream& operator<<(std::ostream& os, const TennisMatch& match);
 
 	class TennisLog
 	{
 	private:
-		int m_num_matches;
+		size_t m_num_matches;
 		TennisMatch* m_matches;
 
 	public:
 		TennisLog();
+		TennisLog(std::string filename);
 		~TennisLog();
+
+		void addMatch(TennisMatch match);
+		TennisLog findMatches();
+
+		TennisMatch operator[](size_t size);
+
+		operator size_t();
+
+		TennisLog(const TennisLog& other); // copy constructor
+		TennisLog(TennisLog&& other); // move constructor
+
+		TennisLog& operator= (const TennisLog& other); // copy assignment
+		TennisLog& operator= (TennisLog&& other) noexcept; // move assignment
 	};
 }
 #endif
